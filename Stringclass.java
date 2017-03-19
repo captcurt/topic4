30afea02f8867aff01c0656f19082e9445225027
captcurt <JCurtner1@my.gcu.edu>
2017-03-18 22:10:47 -0500

    Add files via upload

---
+++ Stringclass.java
@@ -0,0 +1,13 @@
+
+public class Stringclass {
+
+	public Stringclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
